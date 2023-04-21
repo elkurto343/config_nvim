f6052693094174df605a1cd67fc4179c2c5e0192
Kurt Smith <kurt@kaddy.com.au>
2023-04-21 12:27:21 +1000

    initial config

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "simrat39/rust-tools.nvim",
+    },
+    opts = {
+      servers = {
+        rust_analyzer = {},
+      },
+      setup = {
+        rust_analyzer = function(_, opts)
+          require("rust-tools").setup({ server = opts })
+          return true
+        end,
+      },
+    },
+  },
+}
