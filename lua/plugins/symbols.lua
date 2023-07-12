29d987e0d634e4da6b2edb411d0e0ecbdaf7fff4
Kurt Smith <kurt.smith343@gmail.com>
2023-07-12 16:22:03 +1000

    add symbols outline plugin

---
+++ lua/plugins/symbols.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "simrat39/symbols-outline.nvim",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    opts = {
+      auto_preview = true,
+      autofold_depth = 2,
+      show_relative_numbers = true,
+    }
+  },
+}
