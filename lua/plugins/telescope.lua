4d72cba8e595f23023b48b3bb6cb3e1ba8548367
kurt <kurt.smith343@gmail.com>
2024-01-17 20:37:56 +1000

    telescope keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,14 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    keys = {
+      { "<leader><space>", "<cmd>Telescope find_files cwd=false<cr>", desc = "Find Files (cwd)" },
+      { "<leader>.", "<cmd>Telescope find_files cwd=.<cr>", desc = "Find Files (root)" },
+      { "<leader>/", "<cmd>Telescope live_grep cwd=false<cr>", desc = "Grep (cwd)" },
+      { "<leader>sg", "<cmd>Telescope live_grep cwd=false<cr>", desc = "Grep (cwd)" },
+      { "<leader>sG", "<cmd>Telescope live_grep cwd=.<cr>", desc = "Grep (root)" },
+      { "<leader>se", "<cmd>Telescope env<cr>", desc = "Env Vars" },
+    },
   },
   {
     "LinArcX/telescope-env.nvim",
